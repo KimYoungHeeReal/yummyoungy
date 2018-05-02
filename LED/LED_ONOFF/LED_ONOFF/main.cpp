83259edf7fa57c4265635358f5b17abb2cf7b6cf
younghee <lisakim0202@naver.com>
2018-05-02 11:23:40 +0900

    LED 0.5호 켰다껐다

---
+++ LED/LED_ONOFF/LED_ONOFF/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * LED_ONOFF.cpp
+ *
+ * Created: 2018-05-02 오전 10:51:59
+ * Author : stc_160
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    DDRD =0xff;
+	PORTD=0xff;
+    while (1) 
+    {
+		PORTD=0xff;
+		_delay_ms(500);
+		PORTD=0x00;
+		_delay_ms(500);
+    }
+}
+
